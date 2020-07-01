1186174bd7e34e2bd58f2286bd991225a23ba8d9
annnn <voiddipper91@gmail.com>
2020-07-01 13:10:42 +0800

    edit README.txt

--- testindex.py
+++ testindex.py
@@ -1,11 +1,2 @@
-# hi, this is comment
-
-# print ('hello')
-# print ('第三行')
-# print('創建dev分支')
-# print ('創建dev2分支')
-# print('合併dev&dev2到mater')
-# print('test.py')
-# print('新增新資料庫')#master
-# print ('從mater分支修改')#te3
-# print('te3併入te1分支')
+#新手學習跟測試程式語言
+#Learn Coding 
--- workspace/test4.py
+++ workspace/test4.py
@@ -1,17 +1,6 @@
-
 # import random
 
 # a = random.randint(0,99)
 # print(a)
 
 # wait = input('請按任意鍵離開')
-
-
-#test3 commit
-
-int 123
-
-#git 退回測試
-#測試git add -u
-#//測試123
-#push後測試差異
