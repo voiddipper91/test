5f2557a74f2d5b8e2bacc6d7dfb47332ebfd084f
annnn <voiddipper91@gmail.com>
2020-07-01 18:44:33 +0800

    add 九九乘法表

--- workspace/test5.py
+++ workspace/test5.py
@@ -1,16 +1,18 @@
-
-#
+#基本變數
 a=[123,123]
 b=len(a)
 c=sum(a)
-e =1131
+d =1131
+e = float(d)
+f =range(1,6,1)
+g=list(f)
 
-d = float(e)
 
 print ('顯示list a=',a)
 print ('顯示b=',b)
 print ('顯示e=',e)
 print ('顯示c=',c)
 print ('顯示d=',d)
+print (g)
 
 wait=input()
--- workspace/test7.py
+++ workspace/test7.py
@@ -1,1 +1,12 @@
 #隨便寫寫
+#巢狀迴圈
+#99乘法表
+
+for i in range(1, 6, 1):
+    for j in range(1, 2, 1):    	
+    	print (i*"*")
+
+for i in range(1, 10, 1):
+	for j in range(1, 10, 1):
+		print(i,"X",j,"=",i*j)
+wait=input('wait')
